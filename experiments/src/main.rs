34cf73cc16bfcf8e37477266956c5133137cd67c
Justin Lee <justin.huiyun.lee@gmail.com>
2024-02-21 21:38:27 -0800

    ignore

--- experiments/src/main.rs
+++ experiments/src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+#![allow(unused)]
+
+use rand::Rng;
+use std::io;
+
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
+fn main() {
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.14159;
+}
